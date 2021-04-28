2ead8e6b63997f1912c34a07ac64284e8d06a06a
Aayush <shah.aayush3@gmail.com>
2021-04-27 22:07:47 -0700

    multiple changes

--- Website/main.py
+++ Website/main.py
@@ -1,19 +1,24 @@
 from flask import Flask, render_template
+from django.http import HttpResponseRedirect
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def welcome():
     return render_template("welcome.html")
 
 
-@app.route("/microsoft")
-def languages():
+@app.route("/microsoft", methods=['POST', 'GET'])
+def microsoft():
     return render_template("microsoft.html")
 
 
-# @app.route("/card/<int: index>")
-# def card_view(index):
-#     card = db[index]
-#     return render_template("card.html", card=card)
+@app.route("/intel", methods=['POST', 'GET'])
+def intel():
+    return render_template("intel.html")
+
+
+@app.route("/cisco", methods=['POST', 'GET'])
+def cisco():
+    return render_template("cisco.html")
